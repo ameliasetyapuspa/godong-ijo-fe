f45f12dbf7640105f21cbd4c8620d9bafdde80dd
Muhammad Aldryansyah Pamungkas <94610449+AlcodeFun@users.noreply.github.com>
2024-10-23 16:11:34 +0700

    home

--- src/components/CardSignature.vue
+++ src/components/CardSignature.vue
@@ -1,56 +1,59 @@
 <template>
-    <div class="card mb-3" style="max-width: 540px;">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <!-- Use dynamic prop for the image source -->
-          <img :src="imageSrc" class="img-fluid rounded-start" :alt="imageAlt" />
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <!-- Use dynamic prop for the title and text -->
-            <h5 style="font-size: 14px;" class="card-title text-start">{{ cardTitle }}</h5>
-            <p style="font-size: 12px;" class="card-text text-start">{{ cardText }}</p>
-          </div>
+  <div class="card mb-3" style="max-width: 540px">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <!-- Use dynamic prop for the image source -->
+        <img :src="imageSrc" class="img-fluid rounded-start" :alt="imageAlt" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <!-- Use dynamic prop for the title and text -->
+          <h5 style="font-size: 14px" class="card-title text-start">
+            {{ cardTitle }}
+          </h5>
+          <p style="font-size: 12px" class="card-text text-start">
+            {{ cardText }}
+          </p>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: "CardSignature",
-    
-    // Define the props to make the card dynamic
-    props: {
-      imageSrc: {
-        type: String,
-        required: true
-      },
-      imageAlt: {
-        type: String,
-        default: 'Card image'
-      },
-      cardTitle: {
-        type: String,
-        required: true
-      },
-      cardText: {
-        type: String,
-        required: true
-      }
-    }
-  }
-  </script>
+export default {
+  name: "CardSignature",
+
+  // Define the props to make the card dynamic
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+      default: "Card image",
+    },
+    cardTitle: {
+      type: String,
+      required: true,
+    },
+    cardText: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
   
   <style>
-  /* You can add custom styling here if needed */
+/* You can add custom styling here if needed */
 
-  .card{
-    box-shadow: 0px 4px 16px 0px rgba(70, 133, 133, 0.24);
-    border: none !important;
-    width: 300px;
-    height: 100px;
-
-  }
-  </style>
+.card {
+  box-shadow: 0px 4px 16px 0px rgba(70, 133, 133, 0.24);
+  border: none !important;
+  width: 350px;
+  height: 100px;
+}
+</style>
   
---
+++ src/components/CardTestimony.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <div class="img-username d-flex align-items-center">
+      <img
+        width="120"
+        height="120"
+        class="rounded-circle me-2"
+        :src="imgSrc"
+        alt=""
+        style="margin-left: -30px; margin-top: -30px"
+      />
+      <div class="username d-flex flex-column align-items-start">
+        <h4 style="font-size: 16px">{{ fullName }}</h4>
+        <p style="font-size: 14px">{{ instance }}</p>
+      </div>
+    </div>
+
+    <p class="text-start p-3" style="font-size: 12px">“{{ comment }}”</p>
+  </div>
+</template>
+  
+  <script>
+import full from "core-js/full";
+
+export default {
+  name: "CardTestimony",
+  props: {
+    imgSrc: {
+      type: String,
+      required: true,
+      default: "@/assets/Home/signature-1.png",
+    },
+    fullName: {
+      type: String,
+      required: true,
+      default: "Nama Pengunjung",
+    },
+    instance: {
+      type: String,
+      required: true,
+      default: "Nama Instansi",
+    },
+    comment: {
+      type: String,
+      required: true,
+      default:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+  },
+};
+</script>
+  
+  <style  scoped>
+.card {
+  max-width: 480px;
+  min-height: 200px;
+  box-shadow: 0px 4px 16px 0px #4685853d;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 40px;
+}
+</style>
+  
